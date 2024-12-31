e0d8ab1be895049ba2b078e7acca8bca149f321c
2111510646AdiNugroho <2111510646@student.budiluhur.ac.id>
2024-12-31 15:15:58 +0700

    Initial Commit

---
+++ src/main/java/com/example/UasPje2111510646AdiWahyuNugrohoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UasPje2111510646AdiWahyuNugrohoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UasPje211151046AdiWahyuNugrohoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UasPje211151064AdiWahyuNugrohoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UasPje2111510646AdiWahyuNugrohoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
